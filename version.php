420b61dfd99402abc696aa4450a24b979889fba2
Fabio Santos <f4bio.sa@gmail.com>
2024-02-07 11:42:48 -0400

    Create version.php
    Dependencies

---
+++ version.php
@@ -0,0 +1,42 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Aratinga.
+ *
+ * @package    theme_aratinga
+ * @copyright  2024 Fábio Santos - https://voreios.com.br
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();                                                                                                
+                                                                                                                                    
+// This is the version of the plugin.                                                                                               
+$plugin->version = '2024020700';                                                                                                    
+                                                                                                                                    
+// This is the version of Moodle this plugin requires.                                                                              
+$plugin->requires = '2022112800';                                                                                                   
+                                                                                                                                    
+// This is the component name of the plugin - it always starts with 'theme_'                                                        
+// for themes and should be the same as the name of the folder.                                                                     
+$plugin->component = 'theme_aratinga';                                                                                                 
+                                                                                                                                    
+// This is a list of plugins, this plugin depends on (and their versions).                                                          
+$plugin->dependencies = [                                                                                                           
+    'theme_moove' => '2022112801' 
